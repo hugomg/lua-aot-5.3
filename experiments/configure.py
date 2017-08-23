08d533c981f1b1e7b7c4caa4ca0462a01b674835
Hugo Musso Gualandi <hgualandi@inf.puc-rio.br>
2017-08-23 16:49:51 -0300

    Use gcc's -save-temps feature

--- experiments/configure.py
+++ experiments/configure.py
@@ -10,23 +10,25 @@ indir  = "./examples"
 names = []
 lua_files = []
 c_files = []
-s_files = []
 so_files = []
+#intermediate files
+tmp_files = []
 
 for lua_filename in sorted(os.listdir(indir)):
     name, ext = os.path.splitext(lua_filename)
     if ext != '.lua': continue
     lua_files.append( os.path.join(indir, name + '.lua') )
     c_files.append( os.path.join(indir, name + '.c') )
-    s_files.append( os.path.join(indir, name + '.S') )
     so_files.append( os.path.join(indir, name + '.so') )
+    tmp_files.append( os.path.join(indir, name + '.i') )
+    tmp_files.append( os.path.join(indir, name + '.o') )
+    tmp_files.append( os.path.join(indir, name + '.s') )
 
 print('LUASRC=../src')
 print('CC=gcc -std=gnu99')
-print('CFLAGS=-Wall -Wextra -std=c99 -pedantic -fPIC -O2 -Wno-unused-label -g -I$(LUASRC)')
+print('CFLAGS=-Wall -Wextra -pedantic -fPIC -save-temps=obj -O2 -Wno-unused-label -g -I$(LUASRC)')
 print()
 print('C_FILES:=' + " ".join(c_files))
-print('S_FILES:=' + " ".join(s_files))
 print('SO_FILES:=' + " ".join(so_files))
 print()
 print('LUAOT:=$(LUASRC)/luaot')
@@ -39,7 +41,7 @@ print("all: $(SO_FILES)")
 print()
 
 print("clean:")
-print("\t" + "rm -rf -- " + " ".join(c_files + s_files + so_files))
+print("\t" + "rm -rf -- " + " ".join(c_files + so_files + tmp_files))
 print()
 
 for lua_file, c_file in zip(lua_files, c_files):
@@ -47,13 +49,7 @@ for lua_file, c_file in zip(lua_files, c_files):
     print('\t' + '$(LUAOT)' + ' ' + lua_file + ' -o ' + c_file)
     print( )
 
-for c_file, s_file in zip(c_files, s_files):
-    print(s_file + ':' + ' ' + c_file + ' ' + '$(INCLUDES)' )
-    print('\t' + '$(CC) $(CFLAGS) -S ' + ' ' + c_file + ' -o ' + s_file)
+for c_file, so_file in zip(c_files, so_files):
+    print(so_file + ':' + ' ' + c_file + ' ' + '$(INCLUDES)' )
+    print('\t' + '$(CC) $(CFLAGS) -shared ' + ' ' + c_file + ' -o ' + so_file)
     print( )
-
-for s_file, so_file in zip(s_files, so_files):
-    print(so_file + ':' + ' ' + s_file )
-    print('\t' + '$(CC) -shared ' + ' ' + s_file + ' -o ' + so_file)
-    print( )
-
